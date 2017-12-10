bef0fbb85815ab6a79bf77a03abf9e74b14cc16b
David Nichols <epi.sci2020@gmail.com>
2017-12-10 09:44:05 -0500

    different method to produce missing wind data

--- sim_ozone.R
+++ sim_ozone.R
@@ -1,51 +1,41 @@
 set.seed(123)
 library(simstudy)
 library(ggplot2)
+library(GGally)
 
-#create a 2x2 matriz to show solar radiation and ozone
-#with a .52 correlation
-rel <- matrix(c(1, 0.52, 0.52, 1), nrow = 2)
-sol_oz <- genCorData(100, mu = c(120, 15),
-                 sigma = c(50, 5),
-                 corMatrix = rel)
-colnames(sol_oz) <- c("id","sol", "oz")
-sol_oz$sol <- round(sol_oz$sol, 0)
-sol_oz$oz <- round(sol_oz$oz, 0)
-#create wind and tmperature data
-wind_temp <- defData(varname = "wind", 
-               dist = "gamma",
-               formula = 10,
-               variance = 1)
-wind_temp <- defData(wind_temp, varname = "temp", 
-               dist = "normal",
-               formula = 50,
-               variance = 30)
-win_tmp <- genData(100, wind_temp)
-win_tmp$wind <- round(win_tmp$wind, 1)
-win_tmp$temp <- round(win_tmp$temp, 0)
+#create a correlation matrix
+rel <- matrix(c(1, .52, -.10, .15,
+                .52,  1,   0,   0,
+                -.10, 0,   1,   0,
+                .15,  0,   0,   1), nrow = 4)
+rel
 
-#add missing data
-defM <- defMiss(varname = "wind",
-                formula = .15,
-                logit.link = FALSE)
-                
-missMat <- genMiss(win_tmp, defM, idvars = "id")
-win_tmp_m <- genObs(win_tmp, missMat, idvars = "id")
-summary(win_tmp_m)
-
-ozone <- cbind(sol_oz, win_tmp_m)
-ozone <- ozone[,-4]
+ozone <-genCorData(100, mu = c(16, 105,10,50),
+                   sigma = c(6, 50, 5, 15),
+                   corMatrix = rel)
+colnames(ozone) <- c("id","oz", "sol", "wind", "temp")
+ozone <- round(ozone, 0)
+head(ozone)  
+summary(ozone) 
+ozone$wind <- ifelse(ozone$wind < 0, 0, ozone$wind)
 summary(ozone)
 
-coef(lm(oz ~ sol, data = sol_oz))
-ggplot(ozone, aes(x = temp, y = oz)) +
-  geom_point() +
-  geom_abline(intercept = 10, slope = .044)
+#pairs plot for ozone
+ggpairs(ozone[, 2:4],
+        lower = list(continuous = "smooth") ,
+        upper = list(continuous = w_ggally_cor),
+        diag = list(continuous = "densityDiag"))
 
+#create missing wind observations
+zap <- unique(as.integer(runif(22, min = 1, max = 100)))
+zap
+length(zap)
+for(i in zap) {
+  ozone$wind[i] <- NA
+}
+summary(ozone)
+ozone
 
-
-
-  
 
 
 
