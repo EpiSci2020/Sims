8ec23e06687e9c924d3362d31473b88430fb32fa
David Nichols <epi.sci2020@gmail.com>
2017-12-10 00:02:25 -0500

    Added missing data for wind

--- sim_ozone.R
+++ sim_ozone.R
@@ -1,5 +1,6 @@
 set.seed(123)
 library(simstudy)
+library(ggplot2)
 
 #create a 2x2 matriz to show solar radiation and ozone
 #with a .52 correlation
@@ -22,20 +23,25 @@ wind_temp <- defData(wind_temp, varname = "temp",
 win_tmp <- genData(100, wind_temp)
 win_tmp$wind <- round(win_tmp$wind, 1)
 win_tmp$temp <- round(win_tmp$temp, 0)
-ozone <- cbind(sol_oz, win_tmp)
+
+#add missing data
+defM <- defMiss(varname = "wind",
+                formula = .15,
+                logit.link = FALSE)
+                
+missMat <- genMiss(win_tmp, defM, idvars = "id")
+win_tmp_m <- genObs(win_tmp, missMat, idvars = "id")
+summary(win_tmp_m)
+
+ozone <- cbind(sol_oz, win_tmp_m)
 ozone <- ozone[,-4]
+summary(ozone)
 
 coef(lm(oz ~ sol, data = sol_oz))
 ggplot(ozone, aes(x = temp, y = oz)) +
   geom_point() +
   geom_abline(intercept = 10, slope = .044)
 
-
-
-
-
-defM <- defMiss(varname = "w", formula = 10)
-def_obs <- genObs(data, defM, idvars = "id")
 
 
 
